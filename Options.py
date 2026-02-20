da4daa619f21d5cde93aa6c1045c4f28f3bb00e0
RePod <RePod@users.noreply.github.com>
2026-02-20 15:01:35 -0500

    Update Options.py

--- Options.py
+++ Options.py
@@ -29,7 +29,7 @@ class AdditionalSongs(Range):
 
 class DuplicateSongPercentage(Range):
     """
-    After placing required items (leeks and songs), the percentage of remaining filler slots to become duplicate song items.
+    After placing required items (Leeks and songs), the percentage of remaining filler slots to become duplicate song items.
     Duplicate songs are considered Useful thus out of logic and may speed up completion time.
     """
     range_start = 0
@@ -141,7 +141,7 @@ class ScoreGradeNeeded(Choice):
 
 class TotalLeeksAvailable(Range):
     """Controls how many Leeks are added to the pool based on the total number of starting and additional songs.
-    A higher available Leek percentage leads to more consistent game lengths, but individual leeks will be less important.
+    A higher available Leek percentage leads to more consistent game lengths, but individual Leeks will be less important.
     Range is a percentage.
 
     Example: 5 Starting Songs, 40 Additional Songs, 20% Leeks Total = 9 Leeks will be available"""
